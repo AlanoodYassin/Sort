8bc74a2e2c063e3cf3e6754ef0301236b3e2432f
AlanoodYassin <alanood.yassin@hotmail.com>
2020-08-22 23:30:57 +0400

    Quick Sort added to the extra algorithms

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -43,6 +43,39 @@ public class Sort {
             arr[i] = temp; 
         } 
     }
+   int partition(int arr[], int low, int high) 
+    { 
+        int pivot = arr[high];  
+        int i = (low-1); 
+        for (int j=low; j<high; j++) 
+        { 
+            if (arr[j] < pivot) 
+            { 
+                i++; 
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+        int temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    } 
+  
+  
+
+    void Quicksort(int arr[], int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            int pi = partition(arr, low, high); 
+            Quicksort(arr, low, pi-1); 
+            Quicksort(arr, pi+1, high); 
+        } 
+    } 
+   
   
    void printArray(int arr[]) 
     { 
